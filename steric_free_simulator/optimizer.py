proper notebooks, fix storing redundant gradient trees

steric_free_simulator/optimizer.py
@@ -33,11 +33,7 @@ class Optimizer:
         self.dt = None
 
     def plot_observable(self, iteration):
-        if self.sim_class is Simulator:
-            num_steps = int(self.sim_runtime / self.dt)
-            t = np.arange(num_steps) * self.dt
-        elif self.sim_class is VecSim:
-            t = self.sim_observables[iteration]['steps']
+        t = self.sim_observables[iteration]['steps']
         for key in self.sim_observables[iteration].keys():
             if key == 'steps':
                 continue
@@ -86,7 +82,7 @@ class Optimizer:
 
             # preform gradient step
             if i != self.optim_iterations - 1:
-                physics_penalty = torch.sum(100 * F.relu(-1*(self.rn.EA - .1)))  # stops zeroing or negating params
+                physics_penalty = torch.sum(10 * F.relu(-1*(self.rn.EA - .1)))  # stops zeroing or negating params
                 cost = -total_yield + physics_penalty
                 if type(sim) is Simulator:
                     og_params = np.array([p.item() for p in self.rn.get_params()])
@@ -102,9 +98,10 @@ class Optimizer:
                     new_params = self.rn.EA.clone().detach()
 
                 print('param update: ' + str(new_params - og_params))
-                
+
             values = psutil.virtual_memory()
-            if (values.total >> 30) / (1024.0 ** 3) > 16:
+            mem = values.available / (1024.0 ** 3)
+            if mem < 8:
                 # kill program if it uses to much ram
                 return self.rn
             if i == self.optim_iterations - 1:

steric_free_simulator/vec_sim.py
@@ -50,7 +50,10 @@ class VecSim:
         max_poss_yield = torch.min(self.rn.copies_vec[:self.rn.num_monomers].clone())
         while cur_time < self.runtime:
             for obs in self.rn.observables.keys():
-                self.rn.observables[obs][1].append(self.rn.copies_vec[int(obs)].item())
+                try:
+                    self.rn.observables[obs][1].append(self.rn.copies_vec[int(obs)].item())
+                except IndexError:
+                    print('bkpt')
             k = self._compute_constants(self.rn.EA, self.rn.rxn_score_vec)
             copy_prod_vec = self.rn.get_copy_prod_vector()
             rxn_rates = copy_prod_vec * k
@@ -58,9 +61,9 @@ class VecSim:
             step = 1 / total_rate
             rate_step = rxn_rates * step
             delta_copies = torch.matmul(self.rn.M, rate_step)
-            self.rn.copies_vec = self.rn.copies_vec + delta_copies
+            self.rn.copies_vec = torch.max(self.rn.copies_vec + delta_copies, torch.zeros(self.rn.copies_vec.shape))
             cur_time = cur_time + step
-            self.steps.append(cur_time.clone())
+            self.steps.append(cur_time.item())
 
             if len(self.steps) > cutoff:
                 print("WARNING: sim was stopped early due to exceeding set max steps", sys.stderr)