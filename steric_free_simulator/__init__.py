2d38b33c29c9b3082a35520538152012d1542d5b
Spencer R Loggia <spencer.loggia98@gmail.com>
2021-06-16 08:05:12 -0400

    clean up initpy

--- steric_free_simulator/__init__.py
+++ steric_free_simulator/__init__.py
@@ -4,7 +4,7 @@ try:
 except (ModuleNotFoundError, NameError):
     print('EnergyExplorer Module is not available. Check Rosetta installation.', sys.stderr)
 from .reaction_network import ReactionNetwork
-from steric_free_simulator.vectorized_rxn_net import VectorizedRxnNet
+from .vectorized_rxn_net import VectorizedRxnNet
 from .vec_sim import VecSim
 from .optimizer import Optimizer
 from .EqSolver import EquilibriumSolver
