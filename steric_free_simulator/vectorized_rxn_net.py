70f38182abcf341b565e4a7f566ec7a69a0141b5
ajhaver4 <ajhaver4@jhu.edu>
2023-05-24 23:38:42 -0400

    Modified opt of coupled + partial mode to allow for fixed ratio b/w fast and slow interfaces

--- steric_free_simulator/vectorized_rxn_net.py
+++ steric_free_simulator/vectorized_rxn_net.py
@@ -23,7 +23,7 @@ class VectorizedRxnNet:
     units of reaction scores are treated as J * c / mol where c is a user defined scalar
     """
 
-    def __init__(self, rn: ReactionNetwork, assoc_is_param=True, copies_is_param=False, chap_is_param=False,dissoc_is_param=False, dG_is_param=False,cplx_dG=0,mode=None,type='a',dev='cpu',coupling=False,cid={-1:-1}, rxn_coupling=False, rx_cid={-1:-1},std_c=1e6,optim_rates=None):
+    def __init__(self, rn: ReactionNetwork, assoc_is_param=True, copies_is_param=False, chap_is_param=False,dissoc_is_param=False, dG_is_param=False,cplx_dG=0,mode=None,type='a',dev='cpu',coupling=False,cid={-1:-1}, rxn_coupling=False, rx_cid={-1:-1},std_c=1e6,optim_rates=None,slow_rates=None):
         """
 
         :param rn: The reaction network template
@@ -71,6 +71,8 @@ class VectorizedRxnNet:
             self.optim_rates = optim_rates
         else:
             self.partial_opt = False
+
+        self.slow_rates = slow_rates
         self.cid = cid
         self.rx_cid = rn.rxn_cid
         self.coup_map = {}
