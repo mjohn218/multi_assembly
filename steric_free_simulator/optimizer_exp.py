Fixed gradient calculation bug for rategrowth and diversification in conc_beta method

steric_free_simulator/optimizer_exp.py
@@ -479,7 +479,7 @@ class OptimizerExp:
         print("Optimizer State:",self.optimizer.state_dict)
         self.mse_error = []
         time_threshmin=1e-4
-        torch.autograd.set_detect_anomaly(True)
+        # torch.autograd.set_detect_anomaly(True)
 
         for i in range(self.optim_iterations):
             # reset for new simulator
@@ -493,6 +493,11 @@ class OptimizerExp:
             mse=torch.Tensor([0.])
             mse.requires_grad=True
 
+            #This boolean is required for homo_rates and coupling protocol since in each call of simulate() method,
+            #the kon is updated from the corresponsing parameters. Now that works for optimization in other functions, but in this case
+            #since we run multiple simulations in 1 optim iteration, each simulation call will update the kon value. This leads to error since between
+            #each gradient update steps, the parameters cannot be changed. So this boolean ensures that the kon is updated from params_kon only for the 1st simulate call in each iteration.
+            update_kon_bool=True
             for b in range(n_batches):
                 init_conc = float(conc_files_range[b])
                 print("----------------- Starting new batch of Simulation ------------------------------")
@@ -505,10 +510,14 @@ class OptimizerExp:
                 # self.rn.initial_copies = update_copies_vec
                 self.rn.reset()
                 sim.reset()    #Resets the variables of the sim class that are tracked during a simulation.
-
+                # sim = self.sim_class(self.rn,
+                                         # self.sim_runtime,
+                                         # device=self._dev_name)
                 # preform simulation
                 self.optimizer.zero_grad()
-                total_yield,conc_tensor,total_flux = sim.simulate_wrt_expdata(optim,node_str,conc_scale=conc_scale,mod_factor=mod_factor,conc_thresh=conc_thresh,mod_bool=mod_bool,verbose=verbose,yield_species=yield_species)
+                total_yield,conc_tensor,total_flux = sim.simulate_wrt_expdata(optim,node_str,conc_scale=conc_scale,mod_factor=mod_factor,conc_thresh=conc_thresh,mod_bool=mod_bool,verbose=verbose,yield_species=yield_species,update_kon_bool=update_kon_bool)
+                update_kon_bool=False
+
                 print('yield for simulation with  ' + str(init_conc)+"uM" + ' was ' + str(total_yield.item() * 100)[:4] + '%')
 
 
@@ -539,8 +548,10 @@ class OptimizerExp:
 
 
             if self.rn.coupling or self.rn.homo_rates:
+            # if False:
                 new_params = self.rn.params_kon.clone().detach()
-                k = self.rn.params_kon
+                # new_params=self.rn.kon
+                k = self.rn.kon
 
                 print('current params: ' + str(new_params))
                 curr_lr = self.optimizer.state_dict()['param_groups'][0]['lr']
@@ -548,9 +559,10 @@ class OptimizerExp:
 
                 cost = mse_mean #+ physics_penalty
                 cost.backward(retain_graph=True)
-                print('MSE on sim iteration ' + str(i) + ' was ' + str(mse_mean))
+                # print('MSE on sim iteration ' + str(i) + ' was ' + str(mse_mean))
                 print("Reg Penalty: ",physics_penalty)
                 print("Grad: ",self.rn.params_kon.grad)
+
             else:
                 # new_params = self.rn.params_kon.clone().detach()
                 new_params = self.rn.kon.clone().detach()
@@ -561,10 +573,12 @@ class OptimizerExp:
                 physics_penalty = torch.sum(self.reg_penalty * F.relu(-1 * (k - curr_lr * 50))).to(self.dev) #+ torch.sum(10 * F.relu(1 * (k - max_thresh))).to(self.dev)
 
                 cost = mse_mean #+ physics_penalty
-                cost.backward()
+                cost.backward(retain_graph=True)
                 print('MSE on sim iteration ' + str(i) + ' was ' + str(mse_mean))
                 print("Reg Penalty: ",physics_penalty)
                 print("Grad: ",self.rn.kon.grad)
+
+
 
             self.mse_error.append(mse_mean.item())
 
@@ -588,8 +602,10 @@ class OptimizerExp:
                 print("Batch optimization complete")
                 print("Updated params: " + str(new_params))
 
-
             del sim
+
+
+
 
 
 

steric_free_simulator/vec_sim.py
@@ -601,7 +601,7 @@ class VecSim:
             else:
                 return(final_yield.to(self.dev),(t50,t85,t95,t99))
 
-    def simulate_wrt_expdata(self, optim='yield',node_str=None,verbose=False,conc_scale=1.0,mod_factor=1.0,conc_thresh=1e-5,mod_bool=True,yield_species=-1):
+    def simulate_wrt_expdata(self, optim='yield',node_str=None,verbose=False,conc_scale=1.0,mod_factor=1.0,conc_thresh=1e-5,mod_bool=True,yield_species=-1,update_kon_bool=True):
         """
         modifies reaction network
         :return:
@@ -635,7 +635,7 @@ class VecSim:
         t99=-1
 
 
-        if self.rn.coupling:
+        if self.rn.coupling and update_kon_bool:
             #new_kon = torch.zeros(len(self.rn.kon), requires_grad=True).double()
             # print("Coupling")
             if self.rn.partial_opt:
@@ -678,15 +678,16 @@ class VecSim:
                         self.coupled_kon[i] = self.rn.params_kon[self.rn.coup_map[i]]
                 l_k = self.rn.compute_log_constants(self.coupled_kon,self.rn.rxn_score_vec, self._constant)
 
-        elif self.rn.homo_rates:
+        elif self.rn.homo_rates and update_kon_bool:
             counter=0
             for k,rids in self.rn.rxn_class.items():
                 for r in rids:
-                    self.rn.kon[r] = self.rn.params_kon[counter]
+                    self.rn.kon[r] = self.rn.params_kon[counter].clone()
                 counter+=1
             l_k = self.rn.compute_log_constants(self.rn.kon, self.rn.rxn_score_vec, self._constant)
             print("Simulation rates: ",torch.exp(l_k))
-        # l_k = self.rn.compute_log_constants(self.rn.kon, self.rn.rxn_score_vec, self._constant)
+        else:
+            l_k = self.rn.compute_log_constants(self.rn.kon, self.rn.rxn_score_vec, self._constant)
         if verbose:
             print("Simulation rates: ",torch.exp(l_k))
 
@@ -694,8 +695,8 @@ class VecSim:
             conc_counter=1
 
             l_conc_prod_vec = self.rn.get_log_copy_prod_vector()
-
             l_rxn_rates = l_conc_prod_vec + l_k
+
             l_total_rate = torch.logsumexp(l_rxn_rates, dim=0)
             l_step = 0 - l_total_rate
             rate_step = torch.exp(l_rxn_rates + l_step)

steric_free_simulator/vectorized_rxn_net.py
@@ -147,16 +147,26 @@ class VectorizedRxnNet:
                 print("is Leaf: ",self.params_kon[i].is_leaf)
 
         elif self.homo_rates == True:
-            self.params_kon = torch.zeros([len(self.rxn_class.keys())],requires_grad=True).double()
-            self.params_rxn_score_vec = torch.zeros([len(self.rxn_class.keys())]).double()
-            counter=0
-            for k,rid in self.rxn_class.items():
+            if self.partial_opt:
+                c_rxn_count=len(self.optim_rates)
+                self.params_kon = torch.zeros([c_rxn_count],requires_grad=True).double()
+                self.params_rxn_score_vec = torch.zeros([c_rxn_count]).double()
+                rid=0
+                for i in range(c_rxn_count):
+                    self.params_kon[rid] = self.kon.clone().detach()[self.optim_rates[i]]
+                    self.params_rxn_score_vec[rid] = self.rxn_score_vec[self.optim_rates[i]]
+                    rid+=1
+            else:
+                self.params_kon = torch.zeros([len(self.rxn_class.keys())],requires_grad=True).double()
+                self.params_rxn_score_vec = torch.zeros([len(self.rxn_class.keys())]).double()
+                counter=0
+                for k,rid in self.rxn_class.items():
 
-                self.params_kon[counter] = self.kon.clone().detach()[rid[0]]  ##Get the first uid of each class.Set that as the param for that class of rxns
-                self.params_rxn_score_vec[counter] = self.rxn_score_vec[rid[0]]
-                counter+=1
-            self.params_kon.requires_grad_(True)
-            self.initial_params = Tensor(self.params_kon).clone().detach()
+                    self.params_kon[counter] = self.kon.clone().detach()[rid[0]]  ##Get the first uid of each class.Set that as the param for that class of rxns
+                    self.params_rxn_score_vec[counter] = self.rxn_score_vec[rid[0]]
+                    counter+=1
+                self.params_kon.requires_grad_(True)
+                self.initial_params = Tensor(self.params_kon).clone().detach()
 
 
         elif self.bool_rpb: